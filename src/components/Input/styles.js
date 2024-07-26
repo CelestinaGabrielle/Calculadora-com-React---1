8751e961c9b1a675bd0bf2ed72b7b65ba9a4fe7e
Gabrielle_CA <gabriellecelestinaa@gmail.com>
2024-07-26 15:54:38 -0300

    Versão Final

---
+++ src/components/Input/styles.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #595959;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+    font-family: 'Roboto';
+
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #595959;
+        border: 0;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        padding: 0 10px;
+        text-align: right;
+        font-weight: 600;
+        font-size: 30px;
+        font-family: 'Roboto';
+        color: #FFFFFF;
+    }
+`
